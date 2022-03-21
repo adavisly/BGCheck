524c8f71ae241b279b4243f79b5702647eb33db7
adavisly <webayesian@gmail.com>
2022-03-21 14:55:09 +0300

    event task

---
+++ homework/EventsTask/EventsTask/KeyPressing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsTask
+{
+    class KeyPressing
+    {
+        public event EventHandler<char> OnKeyPressed;
+        public void Run()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                char key = Console.ReadKey().KeyChar;
+                OnKeyPressed?.Invoke(this, key);
+                exit = char.ToLower(key) == 'c' ? true : false;
+            }
+        }
+    }
+}
---
+++ homework/EventsTask/EventsTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EventsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyPressing keyP = new KeyPressing();
+
+            keyP.OnKeyPressed += (sender, key) => Console.WriteLine("\nВы нажали на символ " + key);
+            keyP.Run();
+        }
+    }
+}
